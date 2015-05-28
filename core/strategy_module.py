937397316b629ae2a09fac1c83650c8ddc2a4a3b
Camzzz <cameronrdobbs@gmail.com>
2015-05-28 11:04:23 +0100

    Update strategy_module.py

--- core/strategy_module.py
+++ core/strategy_module.py
@@ -1,3 +1,7 @@
 class Strategy:
-    def __init__(self):
-        self.author = "Anonymous"
+    def __init__(self, token, author="anonymous"):
+        assert token in ("X", "O")
+        self.author = author
+        self.token = token
+        
+    
